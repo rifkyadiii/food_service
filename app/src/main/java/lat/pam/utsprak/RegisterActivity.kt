7d4e925150f5061de53d6ccf8f3bbf00cd1555d7
Rifky Adi <90167068+rifkyadiii@users.noreply.github.com>
2024-10-27 23:38:21 +0700

    Perubahan tampilan

---
+++ app/src/main/java/lat/pam/utsprak/RegisterActivity.kt
@@ -0,0 +1,56 @@
+package lat.pam.utsprak
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var inputUsername: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var btnRegister: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        inputUsername = findViewById(R.id.etRegisterUsername)
+        inputPassword = findViewById(R.id.etRegisterPassword)
+        confirmPassword = findViewById(R.id.etConfirmPassword)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val username = inputUsername.text.toString()
+            val password = inputPassword.text.toString()
+            val confirmPwd = confirmPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty() || confirmPwd.isEmpty()) {
+                Toast.makeText(this, "Isi semua kolom", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password != confirmPwd) {
+                Toast.makeText(this, "Password tidak sesuai", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val sharedPref = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+            with (sharedPref.edit()) {
+                putString("username", username)
+                putString("password", password)
+                apply()
+            }
+
+            Toast.makeText(this, "Registrasi berhasil", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
